92aca17e53695719e57ef9c3920e1f9adb834573
creeper521 <19603710129@163.com>
2026-04-24 10:35:39 +0800

    3

--- spring-ai-demo/src/main/java/com/example/springaidemo/controller/QwenController.java
+++ spring-ai-demo/src/main/java/com/example/springaidemo/controller/QwenController.java
@@ -1,4 +1,47 @@
 package com.example.springaidemo.controller;
 
+import org.springframework.ai.chat.messages.SystemMessage;
+import org.springframework.ai.chat.messages.UserMessage;
+import org.springframework.ai.chat.model.ChatModel;
+import org.springframework.ai.chat.model.ChatResponse;
+import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.ai.openai.OpenAiChatModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@RestController
+@RequestMapping("/qwen")
 public class QwenController {
+    @Autowired
+    private OpenAiChatModel chatModel;
+
+    @RequestMapping("/chat")
+    public String chat(String message){
+        return chatModel.call(message);
+    }
+
+    @RequestMapping("/chatWithPrompt")
+    public String chatWithPrompt(String message){
+        Prompt prompt = new Prompt(message);
+        ChatResponse response = chatModel.call(prompt);
+        return response.getResult().getOutput().getText();
+    }
+
+    @RequestMapping("/chatWithRole")
+    public String chatWithRole(String message){
+        SystemMessage systemMessage = new SystemMessage("我是哈基米，是一只凶人的坏猫");
+        UserMessage userMessage = new UserMessage(message);
+        Prompt prompt = new Prompt(systemMessage, userMessage);
+        ChatResponse response = chatModel.call(prompt);
+        return response.getResult().getOutput().getText();
+    }
+
+    @RequestMapping(value = "/stream", produces = "text/html;charset=utf-8")
+    public Flux<String> stream(String message){
+        Prompt prompt = new Prompt(message);
+        Flux<ChatResponse> responseFlux = chatModel.stream(prompt);
+        return responseFlux.map(x->x.getResult().getOutput().getText());
+    }
 }
--- spring-ai-demo/src/main/java/com/example/springaidemo/controller/SseController.java
+++ spring-ai-demo/src/main/java/com/example/springaidemo/controller/SseController.java
@@ -1,4 +1,100 @@
 package com.example.springaidemo.controller;
 
+
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.time.Duration;
+import java.util.Date;
+
+@Slf4j
+@RestController
+@RequestMapping("/sse")
 public class SseController {
+    @RequestMapping("/data")
+    public void data(HttpServletResponse response) throws IOException, InterruptedException{
+        log.info("发出请求：data");
+        response.setContentType("text/event-stream;charset=utf-8");//确定是sse类型
+        PrintWriter writer = response.getWriter();
+        for (int i = 0;i < 10;i++){
+            String s = "data: " + new Date() + "\n\n";
+            writer.write(s);
+            writer.flush();
+            Thread.sleep(1000L);
+        }
+    }
+
+    /**
+     * 控制重连时间
+     * @param response
+     * @throws IOException
+     */
+    @RequestMapping("/retry")
+    public void retry(HttpServletResponse response) throws IOException{
+        log.info("发起请求：retry");
+        response.setContentType("text/event-stream;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        String s = "retry: 2000\n";
+        s += "data: " + new Date() + "\n\n";
+        writer.write(s);
+        writer.flush();
+    }
+
+    /**
+     * 自定义事件
+     * @param response
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @RequestMapping("/event")
+    public void event(HttpServletResponse response) throws IOException, InterruptedException {
+        log.info("发送请求：event");
+        response.setContentType("text/event-stream;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        for(int i = 0;i < 10;i++){
+            String s = "event: foo\n";
+            s += "data: " + new Date() + "\n\n";
+            writer.write(s);
+            writer.flush();
+            Thread.sleep(1000L);
+        }
+    }
+
+    /**
+     * 终止事件
+     * @param response
+     * @throws IOException
+     * @throws InterruptedException
+     */
+    @RequestMapping("/end")
+    public void end(HttpServletResponse response) throws IOException, InterruptedException {
+        log.info("发起请求: event");
+        response.setContentType("text/event-stream;charset=utf-8");
+        PrintWriter writer = response.getWriter();
+        for (int i = 0; i < 10; i++) {
+            String s = "event: foo\n";
+            s += "data: " + new Date() + "\n\n";
+            writer.write(s);
+            writer.flush();
+            Thread.sleep(1000L);
+        }
+        //定义end事件, 表示当前流传输结束
+        writer.write("event: end\ndata: EOF\n\n");
+        writer.flush();
+    }
+
+    /**
+     * 流式打印时间
+     * @return
+     */
+    @RequestMapping(value = "/stream", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<String> stream(){
+        return Flux.interval(Duration.ofSeconds(1)).map(s->new Date().toString());
+    }
 }
